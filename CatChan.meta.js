d7206456acfd3f41e6e5a8edbc40fb1696573c13
DogMan8 <DogMan8@users.noreply.github.com>
2015-02-24 01:43:32 +0900

    Create CatChan.meta.js

---
+++ CatChan.meta.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name CatChan
+// @version 2015.02.14.0
+// @description Cross domain catalog for imageboards
+// @include http*://*krautchan.net/*
+// @include http*://boards.4chan.org/*
+// @include http://*.2chan.net/*
+// @include http*://8chan.co/*
+// @include http*://8ch.net/*
+// @require https://raw.githubusercontent.com/nnnick/Chart.js/master/Chart.js
+// ==/UserScript==
