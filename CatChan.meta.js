dc715535ee8baa9d1579a00a9f677a9339a89709
DogMan8 <DogMan8@users.noreply.github.com>
2016-11-16 22:47:51 +0900

    Update CatChan.meta.js

--- CatChan.meta.js
+++ CatChan.meta.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name CatChan
-// @version 2016.10.16.2
+// @version 2016.10.16.3
 // @description Cross domain catalog for imageboards
 // @include http*://*krautchan.net/*
 // @include http*://boards.4chan.org/*
