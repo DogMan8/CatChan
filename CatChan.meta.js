a253def93339d72bd7c593a1c7246beb07f3ff93
DogMan8 <DogMan8@users.noreply.github.com>
2022-08-14 08:41:47 -0400

    Update CatChan.meta.js

--- CatChan.meta.js
+++ CatChan.meta.js
@@ -1,17 +1,15 @@
 // ==UserScript==
 // @name CatChan
-// @version 2020.01.19.0
+// @version 2021.07.18.3
 // @description Cross domain catalog for imageboards
 // @include http*://*krautchan.net/*
 // @include http*://boards.4chan.org/*
 // @include http*://boards.4channel.org/*
 // @include http*://i.4cdn.org/*
-// @include http*://8chan.co/*
-// @include http*://8ch.net/*
+// @include http*://8kun.top/*
 // @include http*://lainchan.org/*
 // @include http*://lainchan.jp/*
 // @include http*://*meguca.org/*
-// @include http://localhost/*
 // @require https://cdnjs.cloudflare.com/ajax/libs/Chart.js/1.0.1-beta.4/Chart.min.js
 // @updateURL https://raw.github.com/Dogman8/CatChan/master/CatChan.meta.js
 // @grant unsafeWindow
